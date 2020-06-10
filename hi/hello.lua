1a88aa1653f47a90e828b990b2a0b4a9f19909ae
keici <53588633+keici@users.noreply.github.com>
2020-06-09 19:11:55 -0700

    Update hello.lua

--- hi/hello.lua
+++ hi/hello.lua
@@ -2,7 +2,8 @@ local services = {
     Players = game:GetService("Players"),
     ReplicatedStorage = game:GetService("ReplicatedStorage"),
     UIS = game:GetService("UserInputService"),
-    RunService = game:GetService("RunService")
+    RunService = game:GetService("RunService"),
+    CoreGui = game:GetService("CoreGui")
 }
 
 local Carti = {
@@ -12,8 +13,15 @@ local Carti = {
 }
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/keici/lol/master/hi/zypherlib.lua"))()
-local settings = loadstring(game:HttpGet(""))()
+local settings = loadstring(game:HttpGet("https://raw.githubusercontent.com/keici/lol/master/hi/settings.lua"))()
 
+services.UIS.InputBegan:Connect(function(input, gpe)
+    if input.KeyCode == Enum.KeyCode.Insert and not gpe then 
+        services.CoreGui.Poggers.Enabled = not services.CoreGui.Poggers.Enabled
+    end
+end)
+
+-- if retard does script.Disabled lol
 local function getsenvv(scr)
     for i,v in pairs(getgc()) do
         if getfenv(v).script == scr then
