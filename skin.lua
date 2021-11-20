81115deef53d00a1d70cbf7f1fc0c73d2f632ae0
keici <53588633+keici@users.noreply.github.com>
2021-11-19 22:34:18 -0800

    Update skin.lua

--- skin.lua
+++ skin.lua
@@ -1,59 +1,56 @@
-local plr = game.Players.LocalPlayer
-local client = getsenv(plr.PlayerGui.Client)
-local mt = getrawmetatable(game)
-local old = mt.__namecall
-local t = plr.SkinFolder.TFolder:Clone()
-local ct = plr.SkinFolder.CTFolder:Clone()
-plr.SkinFolder.TFolder:Destroy()
-plr.SkinFolder.CTFolder:Destroy()
-t.Parent = plr.SkinFolder
-ct.Parent = plr.SkinFolder
-setreadonly(mt, false)
+local Players = game:GetService("Players")
+local RepStor = game:GetService("ReplicatedStorage")
+local Player = Players.LocalPlayer
+local Client = getsenv(Player.PlayerGui.Client)
+local MT = getrawmetatable(game)
+local OldN = MT.__namecall
+local Skins = {}
 
-mt.__namecall = newcclosure(function(self, ...)
+for i,v in pairs(RepStor.Gloves:GetChildren()) do
+	if v.Name ~= "Models" and v:IsA("Folder") then
+		for i2,v2 in pairs(v:GetChildren()) do
+			table.insert(Skins, {v.Name.."_"..v2.Name})
+		end
+	end
+end
+
+for i,v in pairs(RepStor.Skins:GetChildren()) do
+	for i2,v2 in pairs(v:GetChildren()) do
+		table.insert(Skins, {v.Name.."_"..v2.Name})
+	end	
+end
+
+table.insert(Skins,{"CTKnife_Stock"})
+table.insert(Skins,{"TKnife_Stock"})
+
+setreadonly(MT, false)
+
+MT.__namecall = newcclosure(function(self, ...)
 	local method = getnamecallmethod()
 	local args = {...}
-	if method == "InvokeServer" and self.Name == "Hugh" then
-		return nil
+
+	if method == "InvokeServer" then
+		if self.Name == "Hugh" then
+			return nil
+		end
 	end
 	if method == "FireServer" then
 		if string.len(self.Name) == 38 then 
 			return nil
 		end
-		if self.Name == "DataEvent" then 
+		if self.Name == "DataEvent" and args[1][4] then 
 			if args[1][2] == "CT" or args[1][2] == "Both" then 
-				plr.SkinFolder.CTFolder[args[1][3]].Value = string.split(args[1][4][1], "_")[2]
+				Player.SkinFolder.CTFolder[args[1][3]].Value = string.split(args[1][4][1], "_")[2]
 			end
 			if args[1][2] == "T" or args[1][2] == "Both" then 
-				plr.SkinFolder.TFolder[args[1][3]].Value = string.split(args[1][4][1], "_")[2]
+				Player.SkinFolder.TFolder[args[1][3]].Value = string.split(args[1][4][1], "_")[2]
 			end
 		end
 	end
-	return old(self, unpack(args))
-end)
-setreadonly(mt, true)
-local Skin = {}
 
-for i,v in pairs(game.ReplicatedStorage.Skins:GetChildren()) do
-	for a,b in pairs(v:GetChildren()) do
-		table.insert(Skin, {v.Name.. "_".. b.Name})
-	end
-	table.insert(Skin, {v.Name.. "_Stock"})
-end
-for i,v in pairs(game.ReplicatedStorage.Gloves:GetChildren()) do 
-	if v.Name == "Wraps" or v.Name == "Guts" or v.Name == "MMA" or v.Name == "Wetland" or v.Name == "Ghoul Hex" or v.Name == "Phantom Hex" or v.Name == "Spector Hex" or v.Name == "Orange Hex" or v.Name == "Purple Hex" or v.Name == "Green Hex" then 
-		table.insert(Skin,{"Handwraps_"..v.Name})
-	end
-	if v.Name == "Scapter" or v.Name == "Patch" or v.Name == "Digital" or v.Name == "Crystal" then
-		table.insert(Skin,{"Fingerless Glove_"..v.Name})
-	end
-	if v.Name == "Hazard" or v.Name == "Hallows" or v.Name == "Majesty" or v.Name == "Royal" or v.Name == "Weeb" or v.Name == "CottonTail" or v.Name == "RSL" then 
-		table.insert(Skin,{"Sports Glove_"..v.Name})
-	end
-	if v.Name == "Grim" or v.Name == "Wisk" or v.Name == "Molten" or v.Name == "Kringle" or v.Name == "Racer" then 
-		table.insert(Skin,{"Strapped Glove_"..v.Name})
-	end
-end
-table.insert(Skin,{"CTKnife_Stock"})
-table.insert(Skin,{"TKnife_Stock"})
-client.CurrentInventory = Skin
+	return OldN(self, unpack(args))
+end)
+
+setreadonly(MT, true)
+
+Client.CurrentInventory = Skins
